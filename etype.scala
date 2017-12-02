516be70791c2dc14de32f8c28d2e26b98670d376
邵乾瀚 <cheapl@shaoqianhandeMacBook-Pro.local>
2017-12-02 22:13:33 +0800

    move printVal in to etype

--- etype.scala
+++ etype.scala
@@ -1,4 +1,3 @@
-package etype
 
 class E_AnyVal(in_value: AnyVal){
   var value:AnyVal = in_value
@@ -854,4 +853,20 @@ class E_AnyVal(in_value: AnyVal){
 
 
 class NaN extends E_AnyVal{}
+
+
+object printVal{
+
+  def printVal(x: E_AnyVal){
+    if (x.value.isInstanceOf[Int]) println("Int:"+x.value.asInstanceOf[Int])
+    else if (x.value.isInstanceOf[Long]) println("Long:"+x.value.asInstanceOf[Long])
+    else if (x.value.isInstanceOf[Float]) println("Float:"+x.value.asInstanceOf[Float])
+    else if (x.value.isInstanceOf[Double]) println("Double:"+x.value.asInstanceOf[Double])
+    else if (x.value.isInstanceOf[Char]) println("Double:"+x.value.asInstanceOf[Char])
+    else if (x.value.isInstanceOf[Boolean]) println("Boolean:"+x.value.asInstanceOf[Boolean])
+    else if (x.isInstanceOf[NaN]) println("NaN")
+    else println("Error")
+  }
+
+}
 
